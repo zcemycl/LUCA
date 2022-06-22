68581e05bdd711c263423ba26d4fe658527fd6a1
zcemycl <yui.leung.15@ucl.ac.uk>
2022-06-23 00:15:06 +0100

    add output_shape.txt for testing mobilenetv3

--- tst/tensorflow/model/mobilenetv3_yolov3/test_model.py
+++ tst/tensorflow/model/mobilenetv3_yolov3/test_model.py
@@ -1,3 +1,5 @@
+import os
+
 import tensorflow as tf
 from src.tensorflow.model.mobilenetv3_yolov3.model import MobileNetV3_YoloV3
 
@@ -5,4 +7,16 @@ from src.tensorflow.model.mobilenetv3_yolov3.model import MobileNetV3_YoloV3
 def test_backbone():
     inp = tf.keras.layers.Input([416, 416, 3])
     backbone = MobileNetV3_YoloV3.Backbone(inp)
-    assert backbone.layers[-1].name == "multiply_19"
+    layers = backbone.layers
+    TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+    with open(TEST_DIR + "/output_shape.txt", "r") as f:
+        lines = f.readlines()
+    assert len(layers) == len(lines)
+    for layer, line in zip(layers, lines):
+        nameDim = line.split(",")
+        nameDim[-1] = nameDim[-1].replace("\n", "")
+        dim = list(map(int, nameDim[1:]))
+        if "input" in layer.name:
+            continue
+        assert layer.name == nameDim[0].replace(" ", "")
+        assert list(layer.output_shape)[1:] == dim
