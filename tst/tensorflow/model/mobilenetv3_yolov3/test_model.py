8ce8e6533f7f10cf4cb21fef1d26f64b76eb3606
zcemycl <yui.leung.15@ucl.ac.uk>
2022-06-25 17:34:43 +0100

    add main and parse_args functions to model.py

--- tst/tensorflow/model/mobilenetv3_yolov3/test_model.py
+++ tst/tensorflow/model/mobilenetv3_yolov3/test_model.py
@@ -7,7 +7,11 @@ from typing import Tuple
 import pytest
 
 import tensorflow as tf
-from src.tensorflow.model.mobilenetv3_yolov3.model import MobileNetV3_YoloV3
+from src.tensorflow.model.mobilenetv3_yolov3.model import (
+    MobileNetV3_YoloV3,
+    main,
+    parse_args,
+)
 
 
 @pytest.fixture
@@ -47,3 +51,13 @@ def test_network(inp_net: Tuple[tf.Tensor, tf.keras.Sequential]):
     x = tf.random.normal([1, 416, 416, 3])
     y = model(x)
     assert y.shape == (1, 13, 13, 960)
+
+
+def test_parse_args():
+    args = parse_args(["--debug_model"])
+    assert args.debug_model is True
+
+
+def test_main():
+    args = parse_args([])
+    main(args)
