5e63ef58c156d4584a8d4ee8e773e298003c7e54
zcemycl <yui.leung.15@ucl.ac.uk>
2022-06-26 17:24:09 +0100

    complete mobv3yolov3 network

--- tst/tensorflow/model/mobilenetv3_yolov3/test_model.py
+++ tst/tensorflow/model/mobilenetv3_yolov3/test_model.py
@@ -19,6 +19,14 @@ def inp_net() -> Tuple[tf.Tensor, tf.keras.Sequential]:
     return inp, net
 
 
+@pytest.fixture
+def inp_empty_net() -> Tuple[tf.Tensor, tf.keras.Sequential]:
+    inp = tf.keras.layers.Input([416, 416, 3])
+    args = parse_args(["--anchor_mask", ",".join(["3"] * 9)])
+    net = MobileNetV3_YoloV3(args)
+    return inp, net
+
+
 def test_backbone(inp_net: Tuple[tf.Tensor, tf.keras.Sequential]):
     inp, net = inp_net
     backbone = net.Backbone(inp)
@@ -40,9 +48,23 @@ def test_backbone(inp_net: Tuple[tf.Tensor, tf.keras.Sequential]):
 def test_network(inp_net: Tuple[tf.Tensor, tf.keras.Sequential]):
     _, net = inp_net
     model = net.Network()
-    x = tf.zeros([1, 416, 416, 3])
+    batch = 2
+    x = tf.zeros([batch, 416, 416, 3])
     y = model(x)
-    assert y[-1].shape == (1, 13, 13, 960)
+    assert y[0].shape == (batch, 13, 13, 3, 25)
+    assert y[1].shape == (batch, 26, 26, 3, 25)
+    assert y[2].shape == (batch, 52, 52, 3, 25)
+
+
+def test_empty_network(inp_empty_net: Tuple[tf.Tensor, tf.keras.Sequential]):
+    _, net = inp_empty_net
+    model = net.Network()
+    batch = 2
+    x = tf.zeros([batch, 416, 416, 3])
+    y = model(x)
+    assert y[0].shape == (batch, 13, 13, 0, 25)
+    assert y[1].shape == (batch, 26, 26, 0, 25)
+    assert y[2].shape == (batch, 52, 52, 0, 25)
 
 
 def test_separableConv2D(inp_net: Tuple[tf.Tensor, tf.keras.Sequential]):
@@ -75,6 +97,15 @@ def test_emptyLayer(inp_net: Tuple[tf.Tensor, tf.keras.Sequential]):
     assert y.shape == (batch, *shape)
 
 
+def test_glueLayer(inp_net: Tuple[tf.Tensor, tf.keras.Sequential]):
+    _, net = inp_net
+    batch = 2
+    x = tf.zeros([batch, 13, 13, 512])
+    layer = net.GlueLayer(18, 256)
+    y = layer(x)
+    assert y.shape == (batch, 26, 26, 256)
+
+
 def test_parse_args():
     args = parse_args(["--debug_model"])
     assert args.debug_model is True
@@ -82,4 +113,29 @@ def test_parse_args():
 
 def test_main():
     args = parse_args([])
-    main(args)
+    y = main(args)
+    assert len(y) == 3
+    batch = 1
+    assert y[0].shape == (batch, 13, 13, 3, 25)
+    assert y[1].shape == (batch, 26, 26, 3, 25)
+    assert y[2].shape == (batch, 52, 52, 3, 25)
+
+
+@pytest.mark.parametrize("channels,expected", [(384, 384), (128, 128)])
+def test_make_divisible(
+    inp_net: Tuple[tf.Tensor, tf.keras.Sequential],
+    channels: int,
+    expected: int,
+):
+    _, net = inp_net
+    res = net._make_divisible(channels * 1, 8)
+    assert res == expected
+
+
+def test_skipConv2D(inp_net: Tuple[tf.Tensor, tf.keras.Sequential]):
+    _, net = inp_net
+    batch = 2
+    x = tf.zeros([batch, 52, 52, 40])
+    layer = net.SkipConv2D((1, 1), 1, 128)
+    y = layer(x)
+    assert y.shape == (batch, 52, 52, 128)
