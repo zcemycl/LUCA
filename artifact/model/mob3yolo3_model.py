accf859582a6ef4ffdd665a2f0d455484c405100
zcemycl <yui.leung.15@ucl.ac.uk>
2022-07-21 11:32:21 +0100

    decouple argparse in artifact and tf/model

--- artifact/model/mob3yolo3_model.py
+++ artifact/model/mob3yolo3_model.py
@@ -1,21 +1,19 @@
 import argparse
 import sys
+from typing import List
 
 import matplotlib.patches as ptc
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
-from src.tensorflow.model.mobilenetv3_yolov3.model import (
-    MobileNetV3_YoloV3,
-    parse_args,
-)
+from src.tensorflow.model.mobilenetv3_yolov3.model import MobileNetV3_YoloV3
 
 
 def plotAnchors(
     img: tf.Tensor,
     boxes: tf.Tensor,
     start: int = 0,
-    end: int = 1,
+    end: int = None,
     skip: int = 300,
 ):
     box1 = tf.reshape(boxes, [-1, 4])
@@ -42,14 +40,55 @@ def visualizeAnchors(args: argparse.Namespace):
     model = netObj.Network()
     x = tf.random.normal([1, 416, 416, 3])
     y = model(x)
-    layer_id = 2
-    box_xy, box_wh, box_conf, box_class = netObj.head(layer_id, y[layer_id])
+    box_xy, box_wh, box_conf, box_class = netObj.head(
+        args.layer_id, y[args.layer_id]
+    )
     boxes = netObj.correct_boxes(
         box_xy, box_wh, netObj.input_shape, [416, 416]
     )
     print(boxes.shape)
     x = tf.zeros([1, 416, 416, 3])
-    plotAnchors(x, boxes[:, :, :, 0, :], start=0, end=2704, skip=20)
+    plotAnchors(
+        x, boxes[:, :, :, args.relative_anchor_id, :], start=0, skip=args.skip
+    )
+
+
+def parse_args(args: List[str]) -> argparse.Namespace:
+    p = argparse.ArgumentParser()
+    p.add_argument(
+        "--num_classes",
+        type=int,
+        default=20,
+        help="Number of object classes",
+    )
+    p.add_argument(
+        "--anchor_mask",
+        type=str,
+        default="2,2,2,1,1,1,1,0,0",
+        help="Allocations of anchors for each detection layer",
+    )
+    p.add_argument(
+        "--anchors",
+        type=str,
+        default="""\
+10,13,16,30,33,23,\
+30,61,62,45,59,119,\
+116,90,156,198,373,326""",
+    )
+    p.add_argument("--debug_model", action="store_true")
+    p.add_argument("--alpha", type=int, default=1)
+    p.add_argument(
+        "--max_boxes",
+        type=int,
+        default=20,
+        help="Maximum number of boxes for each class",
+    )
+    p.add_argument("--score_threshold", type=float, default=0.6)
+    p.add_argument("--iou_threshold", type=float, default=0.5)
+    p.add_argument("--layer_id", type=int, default=2)
+    p.add_argument("--relative_anchor_id", type=int, default=0)
+    p.add_argument("--skip", type=int, default=20)
+    return p.parse_args(args)
 
 
 if __name__ == "__main__":
